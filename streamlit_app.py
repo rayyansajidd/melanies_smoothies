b746f8c120d9bc724535b3a1e965659e0b63644e
rayyansajidd <164926513+rayyansajidd@users.noreply.github.com>
2024-08-05 10:58:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,45 +3,40 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-st.write("Choose the fruits you would like in your custom smoothie! ")
+st.title("My Parents New Healthy Diner :cup_with_straw:")
+st.write(
+    """Choose the fruits you want to add to your custom Smoothie
+    """
+)
 
 
-name_on_order = st.text_input("Name on Smoothie")
-st.write("The name on your smoothie will be :", name_on_order)
+title = st.text_input("Name on Smoothies:")
+st.write("The name on smoothie will be:", title)
 
-
-cnx=st.connection("snowfalke")
+cnx = st.connection("snowflake")
 session = cnx.session()
-# session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-'Choose Upto 5 Ingredients: '
-, my_dataframe
+
+    'Choose up to 5 ingredients:'
+    ,my_dataframe
+    ,max_selections=5
 )
-
-# To remove the Bracket when nothing is chosen
 if ingredients_list:
-    ingredients_string = ''
-        
+
+    ingredients_string =''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '  
-        
-    #st.write(ingredients_string)  
+        ingredients_string +=fruit_chosen + ' '
+   # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+title+"""')"""
     time_to_insert = st.button('Submit Order')
-
+    
+    st.write(my_insert_stmt)
+   # st.stop()
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!',icon="✅")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
+        st.success('Your Smoothie is ordered!', icon="✅")
